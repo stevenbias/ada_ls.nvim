c2e595f3df87e0452fda3ad6dc8f3ca9670589de
Steven Bias <bias@adacore.com>
2026-03-13 13:03:07 +0100

    refactor: Move setup plugin from ftplugin to init.lua

---
+++ lua/ada_ls/init.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+function M.setup()
+  vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("AdaLsSetup", { clear = true }),
+    pattern = {
+      "*.ad[bs]",
+    },
+    callback = function()
+      local client = require("ada_ls.utils").get_ada_ls()
+      if client ~= nil then
+        require("ada_ls.project").setup()
+      end
+    end,
+  })
+end
+
+return M
