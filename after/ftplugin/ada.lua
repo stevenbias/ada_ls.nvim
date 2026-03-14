cb92b5932fe67f40b1a1abc0b21fa48ba24b1636
Steven Bias <bias@adacore.com>
2026-03-14 02:33:37 +0100

    feat: Delete <leader>aj keymap in normal mode

--- after/ftplugin/ada.lua
+++ after/ftplugin/ada.lua
@@ -1,3 +1,3 @@
 -- Remove useless default keymaps
-pcall(vim.keymap.del, "i", "<leader>aj", { buffer = true })
+pcall(vim.keymap.del, { "i", "n" }, "<leader>aj", { buffer = true })
 pcall(vim.keymap.del, "i", "<leader>al", { buffer = true })
