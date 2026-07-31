e520db43712f8dc493ddc360cb4e59c3ebbfcfdd
Steven Bias <bias@adacore.com>
2026-07-31 15:40:58 +0200

    chore: remove additional default Ada keymap
    Generated by AI (Claude)

--- after/ftplugin/ada.lua
+++ after/ftplugin/ada.lua
@@ -1,3 +1,4 @@
 -- Remove useless default keymaps
+pcall(vim.keymap.del, "n", "<leader>al", { buffer = true })
 pcall(vim.keymap.del, { "n", "i" }, "<leader>aj", { buffer = true })
 pcall(vim.keymap.del, "i", "<leader>al", { buffer = true })
